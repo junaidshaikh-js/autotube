01688aa00b79cdcfebba841881d2b1c03de777b3
Junaid <sj.shaikhjunaid@gmail.com>
2022-04-09 16:56:27 +0530

    update buttons with button type prop

--- src/component/button/PrimaryBtn/PrimaryBtn.jsx
+++ src/component/button/PrimaryBtn/PrimaryBtn.jsx
@@ -1,10 +1,11 @@
-export const PrimaryBtn = ({ children, cnames }) => {
+export const PrimaryBtn = ({ children, cnames, type = "button" }) => {
   return (
     <button
-      className={`btn btn-primary ${cnames}`}
+      className={`btn btn-auto-primary ${cnames}`}
       style={{
         marginRight: "1rem",
       }}
+      type={type}
     >
       {children}
     </button>
